deb52750a38e3300ebe7cabb7ee43011fe6c70a1
Konstantin Kanellopoulos <k@kanello.de>
2020-11-30 23:29:56 +0100

    start with async

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::fmt;
 use thiserror::Error;
 
+use async_trait::async_trait;
+
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("wrong cas")]
@@ -35,20 +37,21 @@ impl AutoKey for User {
     }
 }
 
-pub trait RepositoryExt<T>: Repository<T>
+#[async_trait(?Send)]
+pub trait RepositoryExt<'a, T>: Repository<T>
 where
     T: AutoKey,
 {
-    fn create_doc(&mut self, doc: T) -> Result<Cas> {
-        self.create(doc.key(), doc)
+    async fn create_doc(&mut self, doc: T) -> Result<Cas> {
+        self.create(doc.key(), doc).await
     }
 
-    fn update_doc(&mut self, doc: T, cas: Cas) -> Result<Cas> {
-        self.update(doc.key(), doc, cas)
+    async fn update_doc(&mut self, doc: T, cas: Cas) -> Result<Cas> {
+        self.update(doc.key(), doc, cas).await
     }
 
-    fn delete_doc(&mut self, doc: T, cas: Cas) -> Result<Option<()>> {
-        self.delete(doc.key(), cas)
+    async fn delete_doc(&mut self, doc: T, cas: Cas) -> Result<Option<()>> {
+        self.delete(doc.key(), cas).await
     }
 }
 impl<D: AutoKey, T: Repository<D>> RepositoryExt<D> for T {}
@@ -64,11 +67,12 @@ impl<T> Document<T> {
     }
 }
 
+#[async_trait(?Send)]
 pub trait Repository<T> {
-    fn create(&mut self, key: String, doc: T) -> Result<Cas>;
-    fn find<K: AsRef<str>>(&mut self, key: K) -> Result<Option<(T, Cas)>>;
-    fn update<K: AsRef<str>>(&mut self, key: K, doc: T, cas: Cas) -> Result<Cas>;
-    fn delete<K: AsRef<str>>(&mut self, key: K, cas: Cas) -> Result<Option<()>>;
+    async fn create(&mut self, key: String, doc: T) -> Result<Cas>;
+    async fn find<K: AsRef<str>>(&mut self, key: K) -> Result<Option<(T, Cas)>>;
+    async fn update<K: AsRef<str>>(&mut self, key: K, doc: T, cas: Cas) -> Result<Cas>;
+    async fn delete<K: AsRef<str>>(&mut self, key: K, cas: Cas) -> Result<Option<()>>;
 }
 
 use std::marker::PhantomData;
@@ -87,28 +91,29 @@ impl<T, R: Repository<T>> Logging<T, R> {
     }
 }
 
+#[async_trait(?Send)]
 impl<T, R: Repository<T>> Repository<T> for Logging<T, R>
 where
     T: std::fmt::Debug,
 {
-    fn create(&mut self, key: String, doc: T) -> Result<Cas> {
+    async fn create(&mut self, key: String, doc: T) -> Result<Cas> {
         println!("CREATE {}: {:?}", key, doc);
-        self.repo.create(key, doc)
+        self.repo.create(key, doc).await
     }
 
-    fn find<K: AsRef<str>>(&mut self, key: K) -> Result<Option<(T, Cas)>> {
+    async fn find<K: AsRef<str>>(&mut self, key: K) -> Result<Option<(T, Cas)>> {
         println!("FIND {}", key.as_ref());
-        self.repo.find(key)
+        self.repo.find(key).await
     }
 
-    fn update<K: AsRef<str>>(&mut self, key: K, doc: T, cas: Cas) -> Result<Cas> {
+    async fn update<K: AsRef<str>>(&mut self, key: K, doc: T, cas: Cas) -> Result<Cas> {
         println!("Update {} {}: {:?}", cas, key.as_ref(), doc);
-        self.repo.update(key, doc, cas)
+        self.repo.update(key, doc, cas).await
     }
 
-    fn delete<K: AsRef<str>>(&mut self, key: K, cas: Cas) -> Result<Option<()>> {
+    async fn delete<K: AsRef<str>>(&mut self, key: K, cas: Cas) -> Result<Option<()>> {
         println!("DELETE {} {}", cas, key.as_ref());
-        self.repo.delete(key, cas)
+        self.repo.delete(key, cas).await
     }
 }
 
